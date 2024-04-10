025167dacf1357cd9269a3fec79da120ea8c4bec
lajo <ezeokefranklin@gmail.com>
2024-04-10 14:48:14 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/index.html"],
+  theme: {
+    extend: {
+      colors:{
+        main: '#0d6efd',
+        whitesmoke :'#f5f5f5'
+      }
+    },
+  },
+  plugins: [],
+}
+
